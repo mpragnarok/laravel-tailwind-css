8d70351b490ed379fd7525b7820800f897457c02
mina.h <mina.h@amaryllo.us>
2020-12-29 16:24:34 +0800

    <init><project with tailwind css>

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>Posty</title>
+</head>
+<body class="bg-gray-200">
+<nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+        <li>
+            <a href="" class="p-3">Home</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Dashboard</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Post</a>
+        </li>
+    </ul>
+    <ul class="flex items-center">
+        <li>
+            <a href="" class="p-3">Mina Huang</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Login</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Register</a>
+        </li>
+        <li>
+            <a href="" class="p-3">Logout</a>
+        </li>
+    </ul>
+</nav>
+
+@yield('content')
+</body>
+</html>
