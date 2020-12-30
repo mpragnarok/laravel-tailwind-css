37b2671efc8daccdf46d03f9db7cb1c34e721b35
mina.h <mina.h@amaryllo.us>
2020-12-30 14:51:36 +0800

    <finished><register and redirect><contorllers and blades><dashboard, Register>

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function index()
     {
         return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        // validation
+        $this->validate($request, [
+            'name' => ['required', 'max:255'],
+            'username' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            // with 'confirmed', Laravel will look up in blade with _confirm name, to make the password are matched
+            'password' => ['required', 'confirmed'],
+        ]);
+        // store user
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        // sign the user in
+        auth()->attempt($request->only('email', 'password')); // return User model
+        // redirect
+        return redirect()->route('dashboard');
+
     }
 }
