d4c62fd8ee08eca8414bb4d5f548b15620621232
mina.h <mina.h@amaryllo.us>
2020-12-31 10:31:47 +0800

    <add><poposts><controller and blade>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,7 +37,15 @@
                     </div>
 
                 </div>
-
+                <div class="mb-4">
+                    <div class="flex items-center">
+                        <input id="remember" name="remember" type="checkbox"
+                               class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+                        <label for="remember" class="ml-2 block text-sm text-gray-900">
+                            Remember me
+                        </label>
+                    </div>
+                </div>
                 <div>
                     <button type="submit"
                             class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,13 +19,13 @@
             <a href="{{route('dashboard')}}" class="p-3">Dashboard</a>
         </li>
         <li>
-            <a href="" class="p-3">Post</a>
+            <a href="{{route('posts')}}" class="p-3">Post</a>
         </li>
     </ul>
     <ul class="flex items-center">
         @auth
             <li>
-                <a href="" class="p-3">Mina Huang</a>
+                <a href="" class="p-3">{{auth()->user()->name}}</a>
             </li>
 
             <li>
