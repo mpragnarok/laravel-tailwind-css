44c78a0e2f238a85199989a7c3aa111292a94aaf
mina.h <mina.h@amaryllo.us>
2020-12-31 11:08:33 +0800

    <add><post><create post with user_id reference>

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,14 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        dd('ok');
+        $this->validate($request, ['body' => 'required']);
+
+
+        // => is the assign operator that is used while creating an array.
+        // -> is the access operator. It accesses an object's value
+        // create functionality
+//        $request->user()->posts()->create(['body' => $request->body]);
+        $request->user()->posts()->create($request->only('body'));
+        return back();
     }
 }
